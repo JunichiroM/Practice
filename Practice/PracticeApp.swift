52b3c111e2faacce741f05740497f64a322d1f86
JunichiroM <sbxn4mxt@yahoo.co.jp>
2024-10-02 16:59:31 +0900

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by apple on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
